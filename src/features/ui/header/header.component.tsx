9be030e26204a877a00eded1b5b8ee4ad768f60b
Rory Magee <roryemagee@gmail.com>
2024-02-27 15:54:50 -0700

    Refactor Header Artvolia title into Button

--- src/features/ui/header/header.component.tsx
+++ src/features/ui/header/header.component.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <h1>Artvolia</h1>
+        <Button><h1>Artvolia</h1></Button>
         <div>
           <Button><h1>AUTH</h1></Button>
           <Button><h1>Notifications</h1></Button>
