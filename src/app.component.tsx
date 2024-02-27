147c5fde52e791c167bb2a0b8f39e3987d7332fe
Rory Magee <roryemagee@gmail.com>
2024-02-27 14:59:28 -0700

    Add absolute path alias for project

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,10 +1,13 @@
 import { JSX } from 'react';
-import './App.css'
+import './app.css'
+
+import Header from '@src/features/ui/header/header.component.tsx';
 
 function App(): JSX.Element {
-
   return (
     <>
+      <h1>Test</h1>
+      <Header />
       <h1>Artvolia</h1>
     </>
   )
