a3239a90339f7a40df3206c8298bd23084152574
Rory Magee <roryemagee@gmail.com>
2024-02-27 16:14:24 -0700

    Add Routing template to App

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,5 +1,6 @@
 import { JSX } from 'react';
 import './app.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from '@src/features/ui/header/header.component.tsx';
 import Main from '@src/features/ui/main/main.component.tsx';
@@ -7,13 +8,19 @@ import Footer from '@src/features/ui/footer/footer.component.tsx';
 
 function App(): JSX.Element {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Main>
-        <h1>Artvolia Page</h1>
+        <Routes>
+          <Route path="/" element={<h1>Dashboard</h1>}/>
+          <Route path="/posts" element={<h1>Posts</h1>}/>
+          <Route path="/profile" element={<h1>Profile</h1>}/>
+          <Route path="/settings" element={<h1>Settings</h1>}/>
+          <Route path="/login" element={<h1>Login</h1>}/>
+        </Routes>
       </Main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
