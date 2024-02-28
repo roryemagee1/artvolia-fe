99c708bf2138cb80ed965974e3877fe4245f6763
Rory Magee <roryemagee@gmail.com>
2024-02-28 13:29:40 -0700

    Add dynamic feed grid resizing

--- src/features/feed/feed/feed.component.tsx
+++ src/features/feed/feed/feed.component.tsx
@@ -4,8 +4,7 @@ import './feed.css';
 import FeedTile from '@src/features/feed/feed-tile/feed-tile.component.tsx';
 
 export default function Feed(): JSX.Element {
-  // const testArray: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-  const testArray: (any)[] = Array(64)
+  const testArray: any[] = Array(64)
   for (let i: number = 0; i < testArray.length; i++) { testArray[i] = i}
   console.log(testArray);
   const outputArray: JSX.Element[] = testArray.map((tile => <FeedTile key={tile}/>))
