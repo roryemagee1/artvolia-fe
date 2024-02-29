3266f834ea78b900f9a05498596b969f1986403d
Rory Magee <roryemagee@gmail.com>
2024-02-29 11:42:08 -0700

    Add code for handling empty array in FeedTile

--- src/features/feed/feed/feed.component.tsx
+++ src/features/feed/feed/feed.component.tsx
@@ -4,10 +4,19 @@ import './feed.css';
 import FeedTile from '@src/features/feed/feed-tile/feed-tile.component.tsx';
 
 export default function Feed(): JSX.Element {
-  const testArray: number[] = Array(64)
+  const testArray: number[] = Array(0)
   for (let i: number = 0; i < testArray.length; i++) { testArray[i] = i}
-  console.log(testArray);
   const outputArray: JSX.Element[] = testArray.map((tile: number): JSX.Element => <FeedTile key={tile}/>)
+  console.log(outputArray);
+
+  if (outputArray.length === 0) {
+    return (
+      <section className="feed">
+        <div className="feed--page-gap"/>
+        <h1>No Posts Found.</h1>
+      </section>
+    )
+  }
   
   return (
     <section className="feed">
