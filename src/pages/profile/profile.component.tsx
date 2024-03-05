1eccce6d7175f3b66b6fccf13ef4a64227bf4c94
Rory Magee <roryemagee@gmail.com>
2024-03-05 15:37:38 -0700

    Add Postbox component

--- src/pages/profile/profile.component.tsx
+++ src/pages/profile/profile.component.tsx
@@ -2,12 +2,14 @@ import { JSX } from 'react';
 import './profile.css';
 
 import ViewProfile from '@src/features/view/view-profile/view-profile/view-profile.component.tsx';
+import PostBox from '@src/features/post/post-box/post-box.component.tsx';
 import Feed from '@src/features/feed/feed/feed.component.tsx';
 
 export default function Profile(): JSX.Element {
   return (
     <section className="profile">
       <ViewProfile />
+      <PostBox />
       <Feed />
     </section>
   )
