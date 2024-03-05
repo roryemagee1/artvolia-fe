f41d506bff0800c393cdb14bda6d467129553659
Rory Magee <roryemagee@gmail.com>
2024-03-04 17:08:40 -0700

    Refactor PublicDashboard component to reflect changes made in Dashboard component

--- src/pages/public-dashboard/public-dashboard.component.tsx
+++ src/pages/public-dashboard/public-dashboard.component.tsx
@@ -2,11 +2,12 @@ import { JSX } from 'react';
 import './public-dashboard.css'
 
 import Feed from '@src/features/feed/feed/feed.component.tsx';
+import FeedSearch from '@src/features/feed/feed-search/feed-search.component.tsx';
 
 export default function PublicDashboard(): JSX.Element {
   return (
     <section className="public-dashboard">
-      <h1>Post/Search/Filter Component</h1>
+      <FeedSearch />
       <Feed />
     </section>
   )
