a3fff077b2c55a167bd755f95a7746974785c70e
Rory Magee <roryemagee@gmail.com>
2024-03-05 16:28:27 -0700

    Add temporary Login NavLink to LoginForm

--- src/features/login/login-form/login-form.component.tsx
+++ src/features/login/login-form/login-form.component.tsx
@@ -1,5 +1,6 @@
 import { JSX } from 'react';
 import './login-form.css'
+import { NavLink } from 'react-router-dom';
 
 import Button from '@src/features/ui/button/button.component.tsx';
 
@@ -11,6 +12,9 @@ export default function LoginForm(): JSX.Element {
         <input placeholder="username"/>
         <input placeholder="password"/>
         <input type="submit" />
+        <NavLink to="/">
+          <h1>Temporary Login</h1>
+        </NavLink>
       </form>
       <Button>Forgot Password?</Button>
       <Button>Sign-up</Button>
